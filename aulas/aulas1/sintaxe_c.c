1897d416345997fa40dc65c1d108ec45551e3a5e
Cauafelipe003 <cauafelipe003@gmail.com>
2024-08-15 00:48:13 +0000

    codigos da aula 1

---
+++ aulas/aulas1/sintaxe_c.c
@@ -0,0 +1,28 @@
+#include <stdio.h>  // inclusao de blibiotecas
+
+int main() {  // funcao principal
+  // seu codigo aqui
+
+  // 1. entrada
+  // levantar meus ingredientes
+  // char, int, float
+  // variaveis
+  // leitura
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 5;
+  segundo_numero = 6;
+    
+
+  // 2. processando
+  // calculo soma, subtracao, multiplicacao, divisao
+  int soma = primeiro_numero + segundo_numero;
+  
+  // 3. saida
+  // escrita
+  printf("A soma de %d com %d eh %d\n", primeiro_numero, segundo_numero, soma);
+  
+  return 0;  // return fim do programa
+
+  
+}
